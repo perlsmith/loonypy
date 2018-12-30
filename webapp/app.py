60e7e37eb54746b15c34d4a2ed64d86619df67de
U-VIDA-LT-AC\acvid <perlsmith@git.com>
2018-12-29 23:40:03 -0600

    fields displayed, no action, no POST

--- webapp/app.py
+++ webapp/app.py
@@ -5,13 +5,13 @@ Created on Sat Dec 29 21:39:08 2018
 @author: acvid
 """
 
-from flask import Flask
+from flask import Flask, render_template
 import mymodule   # just playing along
 
 app = Flask( __name__ ) # creat an object named after active pkg
 
 @app.route('/')     # a decorator? => trigger function defined just below
 def home() -> 'html':
-    return "I am alive!"
+    return render_template('entry.html', page_title='Play with functions')
 
 app.run()
