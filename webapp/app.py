f5736cb2bfa8e339bfbe987a2da2b0f7978fa60e
U-VIDA-LT-AC\acvid <perlsmith@git.com>
2018-12-30 22:23:20 -0600

    working, vowels also

--- webapp/app.py
+++ webapp/app.py
@@ -5,7 +5,7 @@ Created on Sat Dec 29 21:39:08 2018
 @author: acvid
 """
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import mymodule   # just playing along
 
 app = Flask( __name__ ) # creat an object named after active pkg
@@ -13,5 +13,15 @@ app = Flask( __name__ ) # creat an object named after active pkg
 @app.route('/')     # a decorator? => trigger function defined just below
 def home() -> 'html':
     return render_template('entry.html', page_title='Play with functions')
+
+@app.route('/findcommonletters', methods=['POST'])
+def findcommonletters() -> 'html':
+    results = mymodule.find_common_letters( request.form['sentence'], request.form['letters'])
+    return render_template('results.html', results_title='Common Letters:', the_results=results)
+
+@app.route('/countvowels', methods=['POST'])
+def countvowels() -> 'html':
+    results = mymodule.count_vowels( request.form['sentence'] )
+    return render_template('results.html', results_title='Vowels:', the_results=results)
 
 app.run()
